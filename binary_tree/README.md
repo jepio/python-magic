043f15262911dc9f8fe392f35bb01ecf89025fe5
jepio <jeremi.piotrowski@gmail.com>
2014-06-29 23:54:09 +0200

    Added: binary_tree folder.
    The new addition to the python magic repository is my implementation of
    the binary tree. Hopefully I will find the time to expand it and
    implement other kinds of structures. The current binary tree is simple
    yet functional and can be used as is. But obviously the purpose of me
    writing it lies in the educational value more than in the practical.
    There are much better trees out there.

---
+++ binary_tree/README.md
@@ -0,0 +1,18 @@
+Binary Tree
+===========
+
+The binary tree is an interesting data structure. I have built my own and am planning on checking out other interesting data structures in this repository.
+
+To use this class import the class and create a head Node object:
+```python
+from btree import Node
+
+tree = Node(5)
+tree.insert(10)
+tree.insert(3)
+
+print tree
+```
+
+Currently I have implemented searching for a value in the tree, inserting a value into the tree, and printing the tree.
+
---
+++ binary_tree/btree.py
@@ -0,0 +1,52 @@
+""" Self written binary tree. """
+
+
+class Node(object):
+    """
+    A node of a binary tree.
+    """
+
+    def __init__(self, value):
+        """ Create a node with a value. """
+        self.left = None
+        self.right = None
+        self.value = value
+
+    def insert(self, value):
+        """ Insert a value into the tree. """
+        if value > self.value:
+            if self.right is None:
+                self.right = Node(value)
+            else:
+                self.right.insert(value)
+        elif value < self.value:
+            if self.left is None:
+                self.left = Node(value)
+            else:
+                self.left.insert(value)
+
+    def find(self, value):
+        """ Return the value if it exists in the tree, else None. """
+        if value > self.value:
+            if self.right is None:
+                return None
+            else:
+                return self.right.find(value)
+        elif value < self.value:
+            if self.left is None:
+                return None
+            else:
+                return self.left.find(value)
+        else:
+            return self
+
+    def __repr__(self):
+        """ String representation of the tree. """
+        string = []
+        if self.left:
+            string += [repr(self.left)]
+        string += [str(self.value)]
+        if self.right:
+            string += [repr(self.right)]
+        return ','.join(string)
+
